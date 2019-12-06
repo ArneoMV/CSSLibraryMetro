1bedc5783950094b5fd55ec06d7ac15100c5b77f
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2019-12-06 21:17:23 +0100

    commit

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,4 +1,4 @@
-// SCROLLSPY
+// Smooth scroll
 
 const makeNavLinksSmooth = ( ) => {
   const navLinks = document.querySelectorAll( '.nav-link' );
@@ -17,7 +17,7 @@ const makeNavLinksSmooth = ( ) => {
 }
 
 const spyScrolling = ( ) => {
-  const sections = document.querySelectorAll( '.hero-bg' );
+  const sections = document.querySelectorAll('.hero-bg');
 
   window.onscroll = ( ) => {
     const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
@@ -25,11 +25,37 @@ const spyScrolling = ( ) => {
     for ( let s in sections )
       if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
         const id = sections[ s ].id;
-        document.querySelector( '.active' ).classList.remove( 'active' );
-        document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
+        document.querySelector( '.active_NavBar' ).classList.remove( 'active_NavBar' );
+        document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active_NavBar' );
       }
   }  
 }
 
 makeNavLinksSmooth( );
 spyScrolling( );
+
+
+// Scrollspy
+(function() {
+  'use strict';
+
+  const section = document.querySelectorAll('.hero-bg');
+  var sections_NavBar = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections_NavBar[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections_NavBar) {
+      if (sections_NavBar[i] <= scrollPosition) {
+        document.querySelector('.active_NavBar').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active_NavBar');
+      }
+    }
+  };
+})();
+
