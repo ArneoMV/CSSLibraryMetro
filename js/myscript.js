1494f45281f4a2736a0af00e6d2b17e9bee7e218
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2019-12-06 01:56:04 +0100

    commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,4 @@
 /* MAIN SCRIPT */
-
 // MAIN NAVIGATION
 var sidebar = document.getElementById('mainSidebar');
 var checkBox = document.getElementById("mainNav");
@@ -10,18 +9,65 @@ function mainNavigation() {
     // close navigation if hamburger clicked
     if (checkBox.checked == true) {
         sidebar.style.width = "350px";
+        sidebar.style.transition = "900ms";
+        sidebar.style.opacity = "1";
     } else {
         sidebar.style.width = "0";
+        // sidebar.style.transition = "opacity 1300ms ease-out";
+        sidebar.style.transition = "1500ms";
+        sidebar.style.filter = "alpha(opacity=0)";
+        sidebar.style.opacity = "0";
     }
 }
 // link
 sidebar.addEventListener('click', function() {
     // Uncheck
     checkBox.checked = false;
-    sidebar.style.width = "0";
-
+    sidebar.style.width = "0px";
+    sidebar.style.transition = "1500ms";
+    sidebar.style.filter = "alpha(opacity=0)";
+    sidebar.style.opacity = "0";
     console.log('Click just happened');
 });
+
+
+// Add or remove active class
+var elements = document.getElementsByClassName("child");
+
+for(var i = 0; i < elements.length; i++)
+    {
+        elements[i].onclick = function(){
+            
+            // remove class from sibling
+            
+            var el = elements[0];
+            while(el)
+            {
+                if(el.tagName === "DIV"){
+                    //remove class
+                    el.classList.remove("bak");
+                    
+                }
+                // pass to the new sibling
+                el = el.nextSibling;
+            }
+            
+            this.classList.add("bak");  
+        };
+    }
+            
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // syntax Highlight
